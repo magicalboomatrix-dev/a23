771ae9a6fa03f02849bd33cc3d3b25e75f137f6d
Avinash <aviinyou@gmail.com>
2026-04-19 15:35:17 +0530

    meta  data

---
+++ frontend_user_ui/app/download/layout.js
@@ -0,0 +1,25 @@
+export const metadata = {
+  title: 'A23 Satta - Download App',
+  description: 'Download A23 Satta - The most trusted platform for Satta Matka results, live updates, and charts.',
+  openGraph: {
+    title: 'A23 Satta - Download App',
+    description: 'The most trusted platform for Satta Matka results, live updates, and charts.',
+    images: [
+      {
+        url: '/icons/a23_icon_512.png',
+        width: 512,
+        height: 512,
+        alt: 'A23 Satta App',
+      },
+    ],
+    type: 'website',
+  },
+  icons: {
+    icon: '/icons/a23_icon_192.png',
+    apple: '/icons/a23_icon_192.png',
+  },
+};
+
+export default function DownloadLayout({ children }) {
+  return children;
+}
